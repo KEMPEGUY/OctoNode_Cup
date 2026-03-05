fb4c8d102ee0ed9c6839aed1c6832ddb2c53d07e
El-Ikram <ik.elhouiti@gmail.com>
2026-03-05 16:51:12 +0100

    Update metrics.py

--- competition/metrics.py
+++ competition/metrics.py
@@ -4,14 +4,14 @@ from sklearn.metrics import f1_score
 import numpy as np
 
 
-def macro_f1(ml_target, ml_target):
+def macro_f1(y_true, y_pred):
     """
     Compute macro F1 score.
     Ensures integer class labels.
     """
 
-    y_true = np.asarray(ml_target).astype(int)
-    y_pred = np.asarray(ml_target).astype(int)
+    y_true = np.asarray(y_true).astype(int)
+    y_pred = np.asarray(y_pred).astype(int)
 
     if y_pred.ndim > 1:
         y_pred = y_pred.argmax(axis=1)
